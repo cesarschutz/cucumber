dbdaac7747a3a630876c71b43181c6e2dcc48206
Cesa Schutz <cesarschutz340@gmail.com>
2019-12-17 22:17:00 -0300

    contaTeste

--- src/test/java/modelo/ContaTeste.java
+++ src/test/java/modelo/ContaTeste.java
@@ -1,4 +1,11 @@
 package modelo;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:caracteristicas", tags = "@ContaTeste",
+        glue = "cucumber.teste.passos", monochrome = true, dryRun = false)
 public class ContaTeste {
 }
