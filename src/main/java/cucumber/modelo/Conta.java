d21c22c3b4b13dfd5ebabda7b52ad17f8f8804b6
Cesar <cesarschutz340@gmail.com>
2019-12-18 09:47:44 -0300

    contaTeste finalizada

--- src/main/java/cucumber/modelo/Conta.java
+++ src/main/java/cucumber/modelo/Conta.java
@@ -1,4 +1,51 @@
 package cucumber.modelo;
 
 public class Conta {
+    private String dono;
+    private Double limite;
+    private Double saldo;
+    private Integer numero;
+
+    public Conta(String dono, Integer numero, Double limite, Double saldo) {
+        this.dono = dono;
+        this.limite = limite;
+        this.saldo = saldo;
+        this.numero = numero;
+    }
+
+    public void depositar(Double valorDepositado) {
+        this.saldo += valorDepositado;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(Double limite) {
+        this.limite = limite;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
 }
--- src/test/java/cucumber/teste/ContaTeste.java
+++ src/test/java/cucumber/teste/ContaTeste.java
@@ -1,18 +1,15 @@
 package cucumber.teste;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.PendingException;
-import cucumber.api.java.it.Quando;
-import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Entao;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:caracteristicas", tags = "@ContaTeste",
         glue = "modelo.teste.passos", monochrome = true, dryRun = false)
+//features - caminho das features
+//tags - informa qual cenario vai ser executado
+//monochrome - serve para ficar mais legivel os resultados dos testes
+//DRyRun - cria automaticamente os metodos caso não exista na classe de testes
 public class ContaTeste {
-
-
-
 }
